4427a2930fb88d30f0ef717a64a73ab3d14b92ac
jhkim988 <fccva3910@gmail.com>
2022-11-23 07:05:41 +0900

    OOP Decorator - cafe size

--- src/head_first_design_pattern/ch03decorator/Beverage.java
+++ src/head_first_design_pattern/ch03decorator/Beverage.java
@@ -1,11 +1,21 @@
 package head_first_design_pattern.ch03decorator;
 
 public abstract class Beverage {
+    public enum Size {  TALL, GRANDE, VENTI };
+    protected Size size = Size.TALL;
     protected String description;
 
     public abstract double cost();
 
     public String getDescription() {
         return description;
+    }
+
+    public void setSize(Size size) {
+        this.size = size;
+    }
+    
+    public Size getSize() {
+        return this.size;
     }
 }
--- src/head_first_design_pattern/ch03decorator/DarkRoast.java
+++ src/head_first_design_pattern/ch03decorator/DarkRoast.java
@@ -1,13 +1,22 @@
 package head_first_design_pattern.ch03decorator;
 
 public class DarkRoast extends Beverage {
-    public DarkRoast() {
-        description = "다크 로스트";
+    public DarkRoast(Size size) {
+        this.description = "다크 로스트";
+        this.size = size;
     }
 
     @Override
     public double cost() {
-        return 4.1;
+        double cost = 4.1;
+        if (size == Size.TALL) {
+            return cost + 0.1;
+        } else if (size == Size.GRANDE) {
+            return cost + 0.15;
+        } else if (size == Size.VENTI) {
+            return cost + 0.2;
+        }
+        return cost;
     }
     
 }
