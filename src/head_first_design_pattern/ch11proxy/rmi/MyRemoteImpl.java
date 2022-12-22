4fd730e9d43b1e3cb9d35cc269ccbaf97bb45db8
jhkim <jinhankim988@gmail.com>
2022-12-22 13:35:23 +0900

    OOP Learn: Proxy Pattern / RMI

--- src/head_first_design_pattern/ch11proxy/rmi/MyRemoteImpl.java
+++ src/head_first_design_pattern/ch11proxy/rmi/MyRemoteImpl.java
@@ -1,6 +1,5 @@
 package head_first_design_pattern.ch11proxy.rmi;
 
-import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
@@ -8,16 +7,20 @@ import java.rmi.server.UnicastRemoteObject;
 public class MyRemoteImpl extends UnicastRemoteObject implements MyRemote {
 
     public MyRemoteImpl() throws RemoteException {
-        MyRemote service = new MyRemoteImpl();
-        try {
-            Naming.rebind("RemoteHello", service);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
+
     }
 
     @Override
     public String sayHello() throws RemoteException {
         return "Server says, 'Hey'";
+    }
+
+    public static void main(String[] args) {
+        try {
+            MyRemote service = new MyRemoteImpl();
+            Naming.rebind("RemoteHello", service); // 서비스를 등록한다.
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
